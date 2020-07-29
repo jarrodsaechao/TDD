ec1cfaa7846acf853b94f638b93293c44f429bc7
Jarrod Saechao <jarrodsaechao@Jarrods-MacBook-Pro.local>
2020-07-28 20:09:41 -0700

    Now TRUEgit add tests.py check_pwd.py

--- tests.py
+++ tests.py
@@ -33,5 +33,10 @@ class TestCase(unittest.TestCase):
         expected = True
         self.assertEqual(check_pwd(input), expected)
 
+    def test7(self):
+        input = "AquafinaH20  "
+        expected = True
+        self.assertEqual(check_pwd(input), expected)
+
 if __name__ == '__main__':
     unittest.main()
